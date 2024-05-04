f8b26e9c0440ed2d9696d9878d9b71ae964b4db3
Emerson Matos <88755122+Emersomds@users.noreply.github.com>
2024-05-04 08:49:20 -0300

    Implementado formulario de cadastro

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -12,11 +12,15 @@ class CourseController extends Controller
        return view('courses.index');
     }
 
+
+    
     // Visualizar os cursos
     public function show(){
         
         return view('courses.show');
      }
+
+
 
      // Carregar o formulario cadastrar novo curso
     public function create(){
@@ -24,17 +28,30 @@ class CourseController extends Controller
         return view('courses.create');
      }
 
-     // Cadastrar no banco de dados
-    public function store(){
-        
-        dd('cadsatrar  no Banco de Dados');
-     }
+
+
+     // Cadastrar no banco de dados o novo curso
+    public function store(Request $request){
+
+      // Cadastrar no banco de dados na tabela cursos os valores de todos os campos
+      // dd($request->name);
+      Course::create([
+          'name' => $request->name
+      ]);
+      
+      // Redirecionar o usuário, enviar a mensagem de sucesso
+      return redirect()->route('courses.create')->with('success', 'Curso cadastrado com sucesso!');
+  }
  
+
+
      // Carrega o formulario para editar
     public function edit(){
         
         return view('courses.edit');
      }
+
+
 
      //Edita no Banco de Dados
      public function update(){
@@ -42,6 +59,8 @@ class CourseController extends Controller
         dd("Editar o curso no Banco de Dados");
      }
  
+
+
      //Exclui no banco de dados
      public function destroy(){
         
