19f7d8e4803813aaa7ac563fbe61eb19195f6a4e
Emerson Matos <88755122+Emersomds@users.noreply.github.com>
2024-05-06 18:09:31 -0300

    Criado a seeder popular o banco de dados

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Course;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(!Course::where('name', 'Curso de Laravel - 10')->first()){
+            Course::create([
+                'name' => 'Curso de Laravel - 10',
+            ]);
+        }
+
+        if(!Course::where('name', 'Curso de Laravel - 11')->first()){
+            Course::create([
+                'name' => 'Curso de Laravel - 11',
+            ]);
+        }
+
+        if(!Course::where('name', 'Php 8.2')->first()){
+            Course::create([
+                'name' => 'Php 8.2',
+            ]);
+        }
+
+        if(!Course::where('name', 'Html')->first()){
+            Course::create([
+                'name' => 'Html',
+            ]);
+        }    
+    }
+}
